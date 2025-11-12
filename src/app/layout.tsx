a7f01d0b6288398cbe91a0bd7282d96ca34c6e66
ShotoMatsubara <shoto0628matsubara@gmail.com>
2025-11-13 08:36:55 +0900

    feat: フッターコンポーネントを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "@/components/layout/Header";
 import "./globals.css";
+import { Footer } from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
